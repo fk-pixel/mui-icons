bd1af0970d19b0c10e6d171dcba939566e248b8e
fatih kurt <f.kurt@evorhei.de>
2023-11-23 16:00:33 +0100

    change index to mui icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,19 @@ import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 import { GetStaticProps } from 'next';
+import SVG from 'react-inlinesvg';
+import Image from 'next/image';
+import { icons } from '../data/icons';
+import IconDetail from './api/icons/[id]';
+import { useEffect, useState } from 'react';
+
+interface DataProps {
+  id: string;
+  name: string;
+  icon: string;
+  variant: string;
+  category: string;
+}
 
 export default function Home({
   allPostsData,
@@ -15,20 +28,29 @@ export default function Home({
     id: string;
   }[];
 }) {
+  const [data, setData] = useState<DataProps>();
+
+  useEffect(() => {
+    fetch(`/api/icons/50`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  if (!data) return <p>No img data</p>;
+
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>mui icons</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      {/* <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+        <p>(This is a sample website.)</p>
+      </section> */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        {/* <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
@@ -39,7 +61,28 @@ export default function Home({
               </small>
             </li>
           ))}
-        </ul>
+        </ul> */}
+
+        {/* <SVG style={{ color: 'yellow' }} src={'http://localhost:3001/api/icons/1'} /> */}
+        <div key={'img-div'}>
+          <strong>icon_id:</strong> {data.id}
+        </div>
+        <div key={'img-div'}>
+          <strong>icon_name: </strong>
+          {data.name}
+        </div>
+        <div key={'img-div'}>
+          <strong>icon_category:</strong> {data.category}
+        </div>
+        <div key={'img-div'}>
+          <strong>icon_variant:</strong> {data.variant}
+        </div>
+        <div key={'img-div'}>
+          <strong>icon_icon:</strong> <SVG src={data.icon} />
+        </div>
+        {/* <div key={'img-div'}>{json_encode(data.icon)}</div> */}
+        {/* <div>{icons.find((x) => x.id === id).icon}</div> */}
+        {/* <Image alt="test" width={48} height={48} style={{ color: 'yellow' }} src={icons[0].icon} /> */}
       </section>
     </Layout>
   );
@@ -53,3 +96,8 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   };
 };
+
+// export async function getStaticProps(context) {
+//   const query = context.query;
+//   const imgData = await IconDetail(query);
+// }
